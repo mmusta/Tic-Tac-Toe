4c8c4c10ae47e5345d0602512ef1f7d9b7806036
Mücahit Mustafa Usta <mmustafausta@yahoo.com>
2023-02-17 16:54:11 +0300

    chore: Debug some problems using the eslint

--- src/components/Cell.jsx
+++ src/components/Cell.jsx
@@ -1,11 +1,17 @@
 import './styles.css';
+import PropTypes from 'prop-types';
 
 function Cell({ value, handleClick }) {
   return (
-    <span className="cell" onClick={handleClick}>
+    <span tabIndex={0} role="button" className="cell" onClick={handleClick} onKeyDown={() => {}}>
       {value}
     </span>
   );
 }
+
+Cell.propTypes = {
+  value: PropTypes.string.isRequired,
+  handleClick: PropTypes.func.isRequired,
+};
 
 export default Cell;
